53a0f56f284cacda6d7ee0cf8c550bacb1c5e034
Tricky <patrick@holley.dev>
2024-12-04 20:41:37 -0600

    reversed button removal for nav open

--- src/components/ard-nav.ts
+++ src/components/ard-nav.ts
@@ -4,17 +4,43 @@ import {store} from "@store";
 const componentTag = 'ard-nav';
 
 class ArdNav extends HTMLElement {
+    navContainer?: HTMLElement | null;
+    slidingNav?: HTMLElement | null;
+
     constructor() {
         super();
         this.componentTag = componentTag;
         lightRender(this);
     }
 
-    handleSignOut = () => store.logOut();
+    handleDocumentClick = (event: Event) => {
+        if (!this.contains(event.target as Node))  this.toggleNav('remove');
+    }
+
+    handleSignOut = () => {
+        store.logOut();
+    }
+
+    toggleNav = (addOrRemove: 'add' | 'remove') => {
+        [this.slidingNav, this.navContainer].forEach(
+            el => el?.classList[addOrRemove]('open')
+        );
+    }
 
     onRender() {
+        document.addEventListener('click', this.handleDocumentClick);
+        this.slidingNav = this.shadowRoot?.getElementById('sliding-nav');
+        this.navContainer = this.shadowRoot?.getElementById('nav-menu');
+
         (this.shadowRoot?.getElementById('sign-out') as HTMLButtonElement)
             ?.addEventListener('click', this.handleSignOut);
+
+        this.shadowRoot?.getElementById('ardfudge-icon-container')
+            ?.addEventListener('click', () => this.toggleNav('add'));
+    }
+
+    onDismount() {
+        document.removeEventListener('click', this.handleDocumentClick);
     }
 }
 
