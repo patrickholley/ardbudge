2d14b18d104303b479adc66d5ae798ab782881fb
Tricky <patrick@holley.dev>
2024-12-04 20:29:17 -0600

    finished sliding-nav

--- src/components/ard-nav.ts
+++ src/components/ard-nav.ts
@@ -4,43 +4,17 @@ import {store} from "@store";
 const componentTag = 'ard-nav';
 
 class ArdNav extends HTMLElement {
-    navButton?: HTMLElement | null;
-    slidingNav?: HTMLElement | null;
-
     constructor() {
         super();
         this.componentTag = componentTag;
         lightRender(this);
     }
 
-    handleDocumentClick = (event: Event) => {
-        if (!this.contains(event.target as Node))  this.toggleNav('remove');
-    }
-
-    handleSignOut = () => {
-        store.logOut();
-    }
-
-    toggleNav = (addOrRemove: 'add' | 'remove') => {
-        [this.slidingNav, this.navButton].forEach(
-            el => el?.classList[addOrRemove]('open')
-        );
-    }
+    handleSignOut = () => store.logOut();
 
     onRender() {
-        document.addEventListener('click', this.handleDocumentClick);
-        this.slidingNav = this.shadowRoot?.getElementById('sliding-nav');
-        this.navButton = this.shadowRoot?.getElementById('ardfudge-icon-container');
-
         (this.shadowRoot?.getElementById('sign-out') as HTMLButtonElement)
             ?.addEventListener('click', this.handleSignOut);
-
-        (this.navButton as HTMLButtonElement)
-            ?.addEventListener('click', () => this.toggleNav('add'));
-    }
-
-    onDismount() {
-        document.removeEventListener('click', this.handleDocumentClick);
     }
 }
 
